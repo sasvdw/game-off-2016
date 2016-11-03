e981fa3d6c1778f1c6d4ef27702052f544924ca6
Sas van der Westhuizen <sas.vanderwest@entelect.co.za>
2016-11-03 22:17:29 +0200

    Added initial player ship implementation. Basic thruster and rotation is working. Still have to put together some art to use.

---
+++ Unity/GOO2016/Assets/Scripts/Ship/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    private Rigidbody2D body;
+    private float torque;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.torque = 0;
+        this.body = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.torque = Input.GetAxis("Horizontal") * Speed;
+    }
+
+    void FixedUpdate()
+    {
+        body.AddTorque(-torque);
+    }
+}
---
+++ Unity/GOO2016/Assets/Scripts/Ship/Thruster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thruster : MonoBehaviour
+{
+    public float Speed = 10.0f;
+
+    private Rigidbody2D body;
+    private Vector2 force;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.body = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Input.GetAxis("Vertical");
+        var thrust = input > 0 ? input : 0;
+
+        this.force = new Vector2(0, thrust * Speed);
+    }
+
+    void FixedUpdate()
+    {
+        var rot = Quaternion.Euler(0, 0, this.body.rotation);
+        Vector2 force = rot * this.force;
+        this.body.AddForce(force);
+    }
+}
