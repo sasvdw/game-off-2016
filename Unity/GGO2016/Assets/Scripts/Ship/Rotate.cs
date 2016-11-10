fbd5d036efd1d69a3a69077346dbcbbc9864bcba
Sas van der Westhuizen <sas.vanderwest@entelect.co.za>
2016-11-10 22:23:56 +0200

    Missing some changes from initial divorce

--- Unity/GGO2016/Assets/Scripts/Ship/Rotate.cs
+++ Unity/GGO2016/Assets/Scripts/Ship/Rotate.cs
@@ -2,25 +2,24 @@
 
 public class Rotate : MonoBehaviour
 {
-    public float Speed = 10.0f;
     private Rigidbody2D body;
-    private float torque;
 
     // Use this for initialization
     void Start()
     {
-        this.torque = 0;
         this.body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.torque = Input.GetAxis("Horizontal") * Speed;
+        var amount = Input.GetAxis("Horizontal");
+
+        ShipFactory.CurrentShip.SetYaw(amount);
     }
 
     void FixedUpdate()
     {
-        body.AddTorque(-torque);
+        body.AddTorque(-ShipFactory.CurrentShip.ReactionControlSystem.YawTorque);
     }
 }
