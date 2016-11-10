1a2b800cceb622da32eebc799560982dc2f01948
Sas van der Westhuizen <sas.vanderwest@entelect.co.za>
2016-11-10 06:47:59 +0200

    Renamed the project

--- Unity/GGO2016.Domain/Properties/AssemblyInfo.cs
+++ Unity/GGO2016.Domain/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GOO2016.Domain")]
+[assembly: AssemblyTitle("GGO2016.Domain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GOO2016.Domain")]
+[assembly: AssemblyProduct("GGO2016.Domain")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
