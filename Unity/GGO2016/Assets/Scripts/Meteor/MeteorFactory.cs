c927693689e6571c179baf8154e73b61a937826f
Sas van der Westhuizen <sas.vanderwest@entelect.co.za>
2016-11-19 17:29:39 +0200

    Updated factory to randomly distribute an set amount of asteroids within a set radius from vector 0,0

--- Unity/GGO2016/Assets/Scripts/Meteor/MeteorFactory.cs
+++ Unity/GGO2016/Assets/Scripts/Meteor/MeteorFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using GOO2016.Domain.Gravity;
+﻿using GOO2016.Domain.Gravity;
 using UnityEngine;
 
 namespace GGO2016.Unity.Assets.Scripts.Meteor
@@ -7,26 +6,39 @@ namespace GGO2016.Unity.Assets.Scripts.Meteor
     public class MeteorFactory : MonoBehaviour
     {
         public GameObject[] MeteorPrefabs;
+        public int TotalMeteors = 100;
+        public int SpawnRadius = 100;
 
         // Use this for initialization
         void Start ()
         {
             if(this.MeteorPrefabs.Length == 0)
             {
+                Debug.LogError("No MeteorPrefabs set!");
                 return;
             }
 
+            for(var i = 0; i < this.TotalMeteors; i++)
+            {
+                var insideUnitCircle = Random.insideUnitCircle;
+                var meteorPosition = (insideUnitCircle * (this.SpawnRadius - 1)) + insideUnitCircle.normalized;
+                this.SpawnMeteor(meteorPosition);
+            }
+
+            Destroy(this.gameObject);
+        }
+
+        private void SpawnMeteor(Vector2 position)
+        {
             var index = Random.Range(0, this.MeteorPrefabs.Length);
             var meteor = Instantiate(this.MeteorPrefabs[index]);
-            meteor.transform.position = this.transform.position;
+            meteor.transform.position = position;
             var rotation = Random.Range(0.0f, 360.0f);
             meteor.transform.Rotate(0.0f, 0.0f, rotation);
             var massComponent = meteor.GetComponent<IMassComponent>();
             var positionComponent = meteor.GetComponent<IPositionComponent>();
 
             new Body(Simulation.Instance, massComponent, positionComponent);
-
-            Destroy(this.gameObject);
         }
     }
 }
