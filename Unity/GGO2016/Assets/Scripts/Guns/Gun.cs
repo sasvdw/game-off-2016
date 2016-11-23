8c87203b6368c443171b745323c2693626fb58aa
Sas van der Westhuizen <sas.vanderwest@entelect.co.za>
2016-11-23 20:37:05 +0200

    initial turret implementation done

--- Unity/GGO2016/Assets/Scripts/Guns/Gun.cs
+++ Unity/GGO2016/Assets/Scripts/Guns/Gun.cs
@@ -4,15 +4,30 @@ namespace GGO2016.Unity.Assets.Scripts.Guns
 {
     public class Gun : MonoBehaviour
     {
+        public Transform Lazer;
         private Rigidbody2D rigidbody2D;
         private float maxRotation = 30.0f;
         private float gunAngularVelocity = 20.0f;
         private float maxRange = 10.0f;
+        private Transform lazerSpawn;
+        private float energy = 0.0f;
+        private float energyPerShot = 10.0f;
+        private float energyPerSecond = 20.0f;
 
         private void Start()
         {
             this.rigidbody2D = this.GetComponent<Rigidbody2D>();
             this.rigidbody2D.angularVelocity = this.gunAngularVelocity;
+            this.lazerSpawn = this.transform.FindChild("Lazer Spawn");
+        }
+
+        private void Update()
+        {
+            if(this.energy > this.energyPerShot)
+            {
+                return;
+            }
+            this.energy += this.energyPerSecond * Time.deltaTime;
         }
 
         private void FixedUpdate()
@@ -40,6 +55,15 @@ namespace GGO2016.Unity.Assets.Scripts.Guns
             {
                 Debug.DrawLine(this.transform.position, rayDirection * this.maxRange, Color.red);
                 return;
+            }
+
+            if(this.energy > this.energyPerShot)
+            {
+                this.energy -= this.energyPerShot;
+                var eulerAngles = this.transform.eulerAngles;
+                var lazer = Instantiate(this.Lazer, this.lazerSpawn.position, Quaternion.Euler(eulerAngles)) as Transform;
+
+                lazer.GetComponent<Rigidbody2D>().AddForce(rayDirection * 3.0f);
             }
 
             Debug.DrawLine(this.transform.position, rayDirection * this.maxRange, Color.green);
